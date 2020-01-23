c4570dd7238211e98c35787c9bf6d5a122b0bf01
amaralc <amaral.calil@gmail.com>
2020-01-23 12:00:30 -0300

    code: define funcao que guarda valor do input em inputValue

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 var buttonElement = document.querySelector ('#teste button');
+
+// Define function onclick que guarde valor do input (respectivo ao botao clicado) em uma variavel
+var inputValue;
+function getValue(parentNodeId){        
+  var selectorID = "#" + parentNodeId + " input";
+  var inputValue = document.querySelector(selectorID).value;
+  document.querySelector(selectorID).value = null;
+  console.log(inputValue);   
+}
